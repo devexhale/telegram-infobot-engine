4f1eae025bc7c278741dc8a8bf6b2389a9d2b138
stackoverblack <ihorkolybaba@gmail.com>
2025-08-21 20:41:25 +0300

    start

---
+++ tbcf-starter/src/main/java/com/jawisimo/tbcfstarter/Printer.java
@@ -0,0 +1,11 @@
+package com.jawisimo.tbcfstarter;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Printer {
+    public void print() {
+        System.out.println("Hello from starter...");
+    }
+}
---
+++ tbcf-starter/src/test/java/com/jawisimo/tbcfstarter/TbcfStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jawisimo.tbcfstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbcfStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
